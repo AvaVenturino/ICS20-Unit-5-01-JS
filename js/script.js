75bf76841b2dec9c8377271b5af4bf40f1edfbf5
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-04-20 21:32:34 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -10,30 +10,28 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Unit-5-01-JS/sw.js", {
     scope: "/ICS20-Unit-5-01-JS/",
-  });
+  })
 }
 
 /**
  * This function calculates the area of a triangle.
  */
 function button() {
-    // random value generated
-    var y = Math.floor(Math.random() * 7 + 1);
-    var x = document.getElementById("guessField");
-    document.getElementById("submitguess").
-  
-   if(x == y)
-   {    
-       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT")
-   }
-   if(x > y) /* if guessed number is greater than actual number*/ 
-   {    
-       guess++;
-       alert("OOPS SORRY!! TRY A SMALLER NUMBER")
-   }
-   if(x < y) /* if guessed number is smaller than actual number*/
-   {
-       guess++;
-       alert("OOPS SORRY!! TRY A GREATER NUMBER")
-   }
+  // random value generated
+  var y = Math.floor(Math.random() * 7 + 1)
+  var x = document.getElementById("guessField")
+  document.getElementById("submitguess").if(x == y)
+  {
+    alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT")
+  }
+  if (x > y) {
+    /* if guessed number is greater than actual number*/
+    guess++
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER")
+  }
+  if (x < y) {
+    /* if guessed number is smaller than actual number*/
+    guess++
+    alert("OOPS SORRY!! TRY A GREATER NUMBER")
+  }
 }
